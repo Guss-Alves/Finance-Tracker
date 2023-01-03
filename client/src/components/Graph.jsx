d39c928a520cd1b28f8a76f3512f729339d1f553
Guss-Alves <gualvesmagalhaes2020@gmail.com>
2023-01-02 17:01:18 -0800

    updates

--- client/src/components/Graph.jsx
+++ client/src/components/Graph.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState,useEffect } from 'react';
 import { Doughnut } from 'react-chartjs-2'
-import { Chart, ArcElement } from 'chart.js';
+import { Chart, ArcElement, Legend, Tooltip, Title } from 'chart.js';
 import Labels from './Labels';
+import axios from 'axios';
 
 const Graph = () => {
 
-    Chart.register(ArcElement);
+    Chart.register(
+        ArcElement, Legend, Tooltip, Title
+    );
 
     const config = {
-        data:{
+        data: {
+            labels: [
+                'Red',
+                'Blue',
+                'Yellow'
+            ],
             datasets: [{
-                label: 'My First Dataset',
                 data: [300, 50, 100],
                 backgroundColor: [
                     'rgb(255, 99, 132)',
@@ -27,15 +34,38 @@ const Graph = () => {
         }
     }
 
+    //Indian girl video has a way of passing array values to chart throught useState watch again!!!
+    // let [chartData, setChartData] = [{}];
+    
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/loggedUserExpenses', { withCredentials: true })
+        .then(res => {
+            console.log('List res FOR THE CHART ->', res)
+                // setAllExpenses(res.data.results)
+                const categoriesArr = [];
+                const categoriesCosts = [];
+                for(const dataObj of res.data.results){
+                    categoriesArr.push(dataObj.category);
+                    categoriesCosts.push(parseInt(dataObj.cost));
+                }
+                console.log('its here dummy', categoriesArr);
+                console.log('its here COST', categoriesCosts);
+            })
+            .catch(err => {
+                console.log('something went wrong -->', err)
+            })
+        });
+
+
     return (
         <div>
             <h1 className='text-center mb-3'>Expenses Chart</h1>
             <div className="d-flex justify-content-around align-items-center mb-5 ">
-                <div className="graph d-flex justify-content-center m-5" style={{height: '320px'}}>
+                <div className="graph d-flex justify-content-center m-5" style={{ height: '320px' }}>
                     <Doughnut {...config}></Doughnut>
                 </div>
                 <div className="labels">
-                    <Labels/>
+                    <Labels />
                 </div>
             </div>
         </div>
